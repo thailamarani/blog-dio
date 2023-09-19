285ffbd451cfa135bc24899b72d98984e09af3ac
Thaila Marani <thaila_marani@hotmail.com>
2023-09-19 17:58:51 -0300

    Entrega do Projeto - Blog DIO

--- angular-blog/src/app/components/big-card/big-card.component.ts
+++ angular-blog/src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
 
+export class BigCardComponent {
+  @Input()
+  photoCover: string = ""
+  @Input()
+  cardTittle: string = ""
+  @Input()
+  cardDescription: string = ""
+  @Input()
+  Id: string = "0"
 }
---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Meus Contatos",
+    "description": "WhatsApp: (18) 99617-7667 e-mail: thaila_marani@hotmail.com",
+    "photoCover": "https://tse3.mm.bing.net/th?id=OIP.6p6JITAZJBAlQnLvaswkawHaCT&pid=Api&P=0&h=180"
+  },
+  {
+    "id": "2",
+    "title": "Inicio de uma nova jornada",
+    "description": "Em outubro/2023, inicio meu curso de Análise e Desenvolvimento de Sistemas pela faculdade Unimar",
+    "photoCover": "https://faculdadesenacpe.edu.br/v19/wp-content/uploads/2021/06/Estudantes-de-Analise-e-Desenvolvimento-de-Sistemas-participam-do-ProjetAi-1536x1016.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Transição de Carreira",
+    "description": "A transição de carreira pode ser bem definida como sendo o momento no qual um determinado profissional decide realizar uma mudança em relação a sua área de atuação ou, ainda, eu seu posicionamento em uma mesma área. Embora não existam critérios predeterminados para distinguir quando ocorre esse movimento, é possível afirmar que pequenas ou grandes modificações profissionais podem ser consideradas transição de carreira. Um bom exemplo prático que demonstra a transição de carreira estando dentro do mesmo segmento ocorre quando um advogado, que atua majoritariamente na área do direito previdenciário, mas, que por uma razão, pretende atuar com direito de família. Veja-se, neste caso, não houve a necessidade de uma nova graduação. Aqui, será necessário apenas a especialização pela nova área escolhida, dentro do mesmo campo de conhecimento: o direito.",
+    "photoCover": "https://i1.wp.com/s3.amazonaws.com/wordpress-cdn.eadbox.com/2018/04/11141633/26_transicao_carreira_l720x3741.png?fit=720%2C374&ssl=1"
+  },
+  {
+    "id": "4",
+    "title": "Meus Contatos",
+    "description": "WhatsApp: (18) 99617-7667 | e-mail: thaila_marani@hotmail.com",
+    "photoCover": "https://tse3.mm.bing.net/th?id=OIP.6p6JITAZJBAlQnLvaswkawHaCT&pid=Api&P=0&h=180"
+  },
+];
